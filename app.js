63207e5943d81ef811a2648843650ec13768922d
bimbooladele@outlook.com <abimbola.oladele@thebulb.africa>
2020-10-29 14:47:38 +0100

    update: toggle complete feature has been added

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const inputValue = document.querySelector("#input-value")
 const form = document.getElementsByTagName("form")[0]
 const CurrentTodoList = document.querySelector(".todos ul")
 const CompletedTodoList = document.querySelector(".completed ul")
-const currentTodos = [...document.querySelectorAll('.todos ul li')]
+const Tasks = [...document.querySelectorAll('ul')]
+let numberOfCompleted = document.getElementById("number")
 
 const addTodoToUI =(value, element)=>{
     if(value === "") throw new Error("PLeas type something")
     let nodeValue = document.createElement("li")
-    nodeValue.innerHTML = `${value}<i class="fa fa-trash"></i>`
+    nodeValue.innerHTML = `<span><i class="fa fa-square-o"></i> <p>${value}</p></span><i class="fa fa-trash"></i>`
     element.insertBefore(nodeValue, element.childNodes[0])
 }
 const addTodoToArray =(value, array)=>{
@@ -18,6 +19,42 @@ const addTodoToArray =(value, array)=>{
     })
 }
 
+const toggleCompleted = (e, array, currentNode, nextNode)=>{
+    const item = e.path[0]
+    const itemClassArray = [...item.classList]
+    if(itemClassArray.includes("fa-square-o") || itemClassArray.includes("fa-check-square-o")){
+         const text = e.path[2].textContent.trim()
+         const index = array.findIndex(todo => todo.value === text)
+             array[index].completed = !array[index].completed
+             currentNode.removeChild(e.path[2])
+             nextNode.appendChild(e.path[2])
+             const iconPath = e.path[2].childNodes[0].firstChild.classList
+             if(array[index].completed){
+                iconPath.remove("fa-square-o")
+                iconPath.add("fa-check-square-o")
+             }else{
+                iconPath.add("fa-square-o")
+                iconPath.remove("fa-check-square-o")
+             } 
+             getNumberOfCompleted(numberOfCompleted, todos) 
+    }
+}
+const getNumberOfCompleted = (displayNum, array)=>{
+    let number = array.filter(todo => {
+        if(todo.completed) return todo
+    }).length
+    displayNum.textContent = number
+}
+
+const deleteTodo = (e, array, Node)=>{
+    const item = e.path[0]
+        if([...item.classList].includes("fa-trash")){
+                Node.removeChild(e.path[1])
+                const text = e.path[1].textContent.trim()
+                const index = array.findIndex(todo => todo.value === text)
+                array.splice(index, 1)
+            }  
+}
 form.addEventListener("submit", e=>{
     e.preventDefault()
     const todo = inputValue.value.trim()
@@ -25,14 +62,11 @@ form.addEventListener("submit", e=>{
     addTodoToUI(todo, CurrentTodoList)
     inputValue.value = ""
 })
-
-const deleteTodo = (e, array)=>{
-    if(e.srcElement.localName == "i"){
-        CurrentTodoList.removeChild(e.path[1])
-        const text = e.path[1].textContent
-        const index = array.findIndex(todo => todo.value === text)
-        array.splice(index, 1)
-    }
-}
-
-CurrentTodoList.addEventListener("click", (e)=> deleteTodo(e, todos))
+CurrentTodoList.addEventListener("click", (e)=>{
+        deleteTodo(e, todos, CurrentTodoList, CompletedTodoList)
+        toggleCompleted(e, todos, CurrentTodoList, CompletedTodoList)
+})
+CompletedTodoList.addEventListener("click", e=>{
+    deleteTodo(e,todos, CompletedTodoList, CurrentTodoList)
+    toggleCompleted(e, todos, CompletedTodoList, CurrentTodoList)
+})
